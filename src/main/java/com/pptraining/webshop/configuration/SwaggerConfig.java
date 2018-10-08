99a5606ef79b32bdd46d6135c2fa976fadba2c78
kovaxarny <kovaxarny96@gmail.com>
2018-10-08 11:02:59 +0200

    Added Swagger config class Refactored project to match checkstyle constraints

---
+++ src/main/java/com/pptraining/webshop/configuration/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.pptraining.webshop.configuration;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+/**
+ * Swagger configuration class.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error")))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Webshop REST API",
+                "Managing a Webshop.",
+                "API TOS",
+                "Terms of service",
+                new Contact("PPTraining Team", "www.pptraining-webshop.com", "pptraining@company.com"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
+}
