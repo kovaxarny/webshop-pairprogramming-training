302cad3822685028da5a18d7f4ade06b94b58ef0
kovaxarny <kovaxarny96@gmail.com>
2018-10-15 11:52:55 +0200

    Added Facebook login with OAuth2

--- src/main/java/com/pptraining/webshop/WebshopApplication.java
+++ src/main/java/com/pptraining/webshop/WebshopApplication.java
@@ -1,16 +1,15 @@
 package com.pptraining.webshop;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
+@EnableOAuth2Client
 public class WebshopApplication {
-
-    private static final Logger LOGGER = LogManager.getLogger(WebshopApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(WebshopApplication.class, args);
+
     }
 }
--- src/main/java/com/pptraining/webshop/api/EchoController.java
+++ src/main/java/com/pptraining/webshop/api/EchoController.java
@@ -1,5 +1,8 @@
 package com.pptraining.webshop.api;
 
+import com.pptraining.webshop.WebshopApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -7,12 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * Controller class for testing purposes.
+ */
+//TODO remove this class later.
+
 @Controller
-@RequestMapping (path = "/api/v1/echo")
+@RequestMapping(path = "/api/v1/echo")
 public class EchoController {
 
+    private static final Logger LOGGER = LogManager.getLogger(WebshopApplication.class);
+
     @GetMapping(path = "/greeting")
-    public ResponseEntity<String> greeting(@RequestParam final String name){
+    public ResponseEntity<String> greeting(@RequestParam final String name) {
+        LOGGER.info("Greeting sent to " + name);
         return new ResponseEntity<>("Hello " + name, HttpStatus.OK);
     }
 
