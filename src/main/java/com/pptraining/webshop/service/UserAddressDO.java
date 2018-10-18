c5fe22a249f074c84d169f14bd930de4cfc96463
kovaxarny <kovaxarny96@gmail.com>
2018-10-18 20:59:48 +0200

    EchoController cleared. UserAddressDO simplified. UserRepository extended with findByUsername method.

--- src/main/java/com/pptraining/webshop/service/UserAddressDO.java
+++ src/main/java/com/pptraining/webshop/service/UserAddressDO.java
@@ -3,17 +3,19 @@ package com.pptraining.webshop.service;
 import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @ToString(exclude = "id")
+
 @EqualsAndHashCode(exclude = "id")
 @Getter
 @Setter
 @Entity
 @Table(name = "user_address")
-public class UserAddressDO {
+public class UserAddressDO implements Serializable{
     @Id
     @Setter(AccessLevel.PRIVATE)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,8 +35,4 @@ public class UserAddressDO {
 
     @Column(name = "house_number", length = 32)
     private String houseNumber;
-
-//    @OneToOne(fetch = FetchType.LAZY, optional = false)
-//    @JoinColumn(name = "user_id", nullable = false)
-//    private UserDO user;
 }
--- src/main/java/com/pptraining/webshop/service/UserRepository.java
+++ src/main/java/com/pptraining/webshop/service/UserRepository.java
@@ -3,6 +3,10 @@ package com.pptraining.webshop.service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserDO, Long> {
+
+    Optional<UserDO> findByUsername(String username);
 }
